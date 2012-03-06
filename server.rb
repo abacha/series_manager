c56a5e740d018236686b7cc3b13986dcec93ee0a
Adriano Bacha <abacha@gmail.com>
2012-03-06 15:16:22 -0400

    new version gem

--- server.rb
+++ server.rb
@@ -4,15 +4,24 @@ require 'series_timer'
 
 get "/next/*" do
   serie = params[:splat].join(" ")
-  `series_timer -c "#{serie}"`
+  SeriesTimer::SeriesManager.countdown(serie)
 end
 
 get "/all/*" do
   serie = params[:splat].join(" ")
-  `series_timer -a "#{serie}"`.gsub("\n", "<br>")
+  SeriesTimer::SeriesManager.all(serie).join("<br>")
 end
 
 get "/last/*" do
   serie = params[:splat].join(" ")
-  `series_timer -l "#{serie}"`
+  SeriesTimer::SeriesManager.last_episode(serie)
+end
+
+get "/serie/*" do
+  serie = params[:splat].join(" ")
+  "Last episode on air: #{SeriesTimer::SeriesManager.last_episode(serie)}<br>
+  <br><br> 
+  Next episode on air: #{SeriesTimer::SeriesManager.countdown(serie)}<br>
+  <br><br> 
+  #{SeriesTimer::SeriesManager.all(serie).join("<br>")}"
 end
